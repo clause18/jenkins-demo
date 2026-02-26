5f2e77e5d12a6292b689a7ec5bff6f63335dd03d
clause18 <ved.vis.rt23@dypatil.edu>
2026-02-26 16:55:04 +0530

    Add hello world program

---
+++ HelloWorld.java
@@ -0,0 +1,6 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello from Jenkins! This is my first build job.");
+        System.out.println("Jenkins successfully integrated with GitHub!");
+    }
+}
