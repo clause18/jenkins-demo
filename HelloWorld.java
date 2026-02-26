3c1f5af4d4d3cd1ef671445765d01ea3fdeb2f25
clause18 <ved.vis.rt23@dypatil.edu>
2026-02-26 18:40:47 +0530

    Delete HelloWorld.java

--- HelloWorld.java
+++
@@ -1,7 +0,0 @@
-public class HelloWorld {
-    public static void main(String[] args) {
-        System.out.println("Hello from Jenkins! This is my first build job.");
-        System.out.println("Jenkins successfully integrated with GitHub!");
-        System.out.println("This line was added after first build!");
-    }
-}
